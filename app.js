0d07c0b94a1e84449ae0fc7dca31090ec8032ba5
Manthan harale <126886816+manthan20021@users.noreply.github.com>
2024-06-26 07:10:44 -0700

    all set

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// import React from "react"
-// import ReactDOM from "react-dom/client"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 // const hed = document.getElementById('hed')
 // hed.innerHTML = "hwllo react"
@@ -11,10 +12,10 @@
 // root.appendChild(hed)
 
 
-// thsi parnathises is use to geive the atrubut to our tag,like
+//thsi parnathises is use to geive the atrubut to our tag,like
 // const heding = React.createElement('h1', {id:"hed"}, "react frist code")
-//     const root = ReactDOM.createRoot(document.getElementById("root"))
-//     root.render(heding)
+//     const myRoot = ReactDOM.createRoot(document.getElementById("root"))
+//     myRoot.render(heding)
 
 // const div = React.createElement(
 //     "div", {id: "parent"}, 
@@ -31,3 +32,115 @@
 
 //         const root = ReactDOM.createRoot(document.getElementById("root"))
 //         root.render(div)
+
+
+// const heding = React.createElement(
+//     'h1',
+//     {id:"heding"}, 
+//     "ram ram React"
+// )
+// console.log(heding);
+
+// const jsxHed = <h1 id="hed" className="heding">hello</h1>
+
+// console.log(jsxHed);
+
+
+// const FristCom = () => {
+
+//     return(
+//         <div>
+//             <Heding/>
+//             <Heding/>
+//             <Heding/>
+//             <Heding/>
+//             <h3>he iam manthan harale and iam froma akola maharashtra i have complited my bcom in 2023 now I wont to go on porgraming paht</h3>
+//         </div>
+//     )
+// }
+
+// const Heding = () => ( 
+    
+//     <h1>INTRODUCTION</h1>
+// )
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(<FristCom/>)
+
+// const div = React.createElement(
+//     'div',{className:"titell"}, React.createElement(
+//         'h1',{}, "hello h1"), React.createElement(
+//             'h2',{}, "hello h2"), React.createElement(
+//                 'h3',{}, "hello h3"))
+
+
+
+// const div = React.createElement(
+//     'div', {className:"titell"}, React.createElement(
+//         'h1',{}, React.createElement(
+//             'h2',{},React.createElement(
+//                 'h3',{}, "hello iam hear"
+//             )
+//         )
+//     )
+// )
+
+//                 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+//                 root.render(div)
+
+
+// const Hello = <div className="titell">
+//     <h1>hello manthan</h1>
+//     <h2>hello sanchit</h2>
+//     <h3>hello vinit</h3>
+// </div>
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// root.render(Hello)
+
+
+
+// const Hello = () => {
+
+//     return(
+        
+//         <div className="tatill">
+          
+//             <h1 id="m1">apple</h1>
+//             <h2 id="m2">mango</h2>
+//             <h3 id="m3">banana</h3>
+//         </div>
+//     )
+// }
+
+// const Logo = () => {
+//     return <h1 className="h1">LOGO</h1>
+// }
+
+// const Search = () => {
+//     return<input id="sarch" placeholder="search"/>
+// }
+
+// const Icon = () => {
+//     return <h1 className="icon">Icon</h1>
+// }
+
+// const Header = () => {
+//     return(
+//         <div id="navbar">
+//             <Logo/>
+//             <Search/>
+//             <Icon/>
+//         </div>
+
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// root.render(<Header/>)
